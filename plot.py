18bb734c6edf65763e41b4d94903a1a32edd5cf8
rcburnet <cnadiandevl@gmail.com>
2018-03-22 22:54:21 -0400

    updated plot to look nicer

--- plot.py
+++ plot.py
@@ -13,8 +13,6 @@ years = mdates.YearLocator()
 months = mdates.MonthLocator()
 days = mdates.DayLocator()
 
-# Eg datetime format: datetime.datetime.strptime('201701020748','%Y%m%d%H%M')
-
 # Open weight file of dates and weights
 weight_file = open('weight.txt')
 weight_lines = weight_file.readlines()
@@ -27,29 +25,28 @@ del weight_lines[0]
 date_time = []
 date_time_fast = []
 date_time_feast = []
-date_time_partial_fast = []
+date_time_heavy_caloric_restriction = []
 weight = []
 weight_fast = []
 weight_feast = []
-weight_partial_fast = []
+weight_heavy_caloric_restriction = []
 fast = []
 for i in range(len(weight_lines)):
     weight_lines[i] = weight_lines[i].split(';')
     date_time.append(datetime.datetime.strptime(weight_lines[i][0], '%Y%m%d%H%M'))
     weight.append(float(weight_lines[i][1]))
-    if float(weight_lines[i][2]) == 0.0:
+    if int(weight_lines[i][2]) == 0:
         weight_feast.append(float(weight_lines[i][1]))
         date_time_feast.append(datetime.datetime.strptime(weight_lines[i][0], '%Y%m%d%H%M'))
-    elif float(weight_lines[i][2]) == 2.0:
+    elif int(weight_lines[i][2]) == 2:
         weight_fast.append(float(weight_lines[i][1]))
         date_time_fast.append(datetime.datetime.strptime(weight_lines[i][0], '%Y%m%d%H%M'))
     else:
-        weight_partial_fast.append(float(weight_lines[i][1]))
-        date_time_partial_fast.append(datetime.datetime.strptime(weight_lines[i][0], '%Y%m%d%H%M'))
+        weight_heavy_caloric_restriction.append(float(weight_lines[i][1]))
+        date_time_heavy_caloric_restriction.append(datetime.datetime.strptime(weight_lines[i][0], '%Y%m%d%H%M'))
     fast.append(weight_lines[i][2][:-1])
-    
 
-# Moving averge functon, taken striaight from:
+# Moving average functon, taken straight from:
 # https://gordoncluster.wordpress.com/2014/02/13/python-numpy-how-to-generate-moving-averages-efficiently-part-2/
 def movingaverage (values, window):
     weights = np.repeat(1.0, window)/window
@@ -61,29 +58,25 @@ rolling_mean = movingaverage(weight, rolling_window)
 
 # Plot everything:
 fig, ax = plt.subplots()
-# Plot lines connecting rolling mean and weight for each day vertically. Plot
-# this first so that it lies underneath the other pots
-for i in range(len(rolling_mean)):
-    plt.plot([date_time[rolling_window+i-1],date_time[rolling_window+i-1]],[rolling_mean[i],weight[rolling_window+i-1]],'k--')
-# Plot weight over time
-plt.plot(date_time, weight,'k')
-plt.plot(date_time_fast, weight_fast,'ko', markerfacecolor = 'white', markersize = 5, label = 'Fast')
-plt.plot(date_time_feast, weight_feast,'ko', markersize = 5, label = 'Feast')
-plt.plot(date_time_partial_fast, weight_partial_fast,'ko', markerfacecolor = 'grey', markersize = 5, label = 'Heavy Caloric Restriction')
+# Plot shaded regions above and below trendline
+plt.fill_between(date_time[rolling_window-1:], rolling_mean, weight[rolling_window-1:], where=rolling_mean >= weight[rolling_window-1:], facecolor='blue', alpha=0.5, interpolate=True)
+plt.fill_between(date_time[rolling_window-1:], rolling_mean, weight[rolling_window-1:], where=rolling_mean <= weight[rolling_window-1:], facecolor='red', alpha=0.5, interpolate=True)
 # Plot rolling mean over time
-plt.plot(date_time[rolling_window-1:],rolling_mean,'b',label = str(rolling_window)+'-day rolling mean')
-#plt.xlim(min(date_time),max(date_time))
-#plt.ylim(140,max(weight)+10)
+plt.plot(date_time[rolling_window-1:], rolling_mean, 'green', alpha=0.5, label=str(rolling_window)+'-day rolling mean')
+# Plot weight over time
+plt.plot(date_time_fast, weight_fast, 'ko', markerfacecolor='white', markersize=5, label='Fast')
+plt.plot(date_time_feast, weight_feast, 'ko', markersize=5, label='Feast')
+plt.plot(date_time_heavy_caloric_restriction, weight_heavy_caloric_restriction, 'ko', markerfacecolor='grey', markersize=5, label='Heavy Caloric Restriction')
+# Plot legend
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 # Format x-axis
-#fig.autofmt_xdate()
 ax.xaxis.set_major_formatter(myFmt_months)
-#ax.xaxis.set_minor_formatter(myFmt_days)
 ax.xaxis.set_major_locator(months)
 ax.xaxis.set_minor_locator(days)
-#ax.xaxis.set_tick_params(which='major', pad=15)
-plt.title('Daily weight during IF')
+# Fill in text for plot
+plt.title('Daily weight')
 plt.ylabel('Weight (lbs)')
 plt.xlabel('Date')
+# save plot and close
 plt.savefig('weight.png', bbox_inches='tight')
 plt.close()
